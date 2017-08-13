1b67755aa6631b657e81c67b3b575f8d71eeadfa
mirotik666 <tris151316@gmail.com>
2017-08-13 16:47:39 +0300

    de

---
+++ src/body.vue
@@ -0,0 +1,36 @@
+<script>
+import ImageSlider from './components/image-slider/image-slider.vue';
+
+export default {
+    components:{
+      ImageSlider
+    }
+  };
+</script>
+<style>
+html,body { 
+  width:100%;
+  height:100%;
+  margin:0;
+  padding:0;
+}
+
+body { 
+  box-sizing:border-box;
+  padding: 20px;
+}
+.image-slider{
+  height: 300px;
+}
+</style> 
+
+
+
+
+
+<template>
+  <body>
+    Application started
+    <image-slider />
+  </body>
+</template>
---
+++ src/components/image-slider/image-slider.vue
@@ -0,0 +1,62 @@
+<script> 
+export default {
+    component:{},
+    props:{
+        image: Array 
+    },
+    data(){
+        return{
+            index: 0
+        };
+    },
+    computed:{
+        length(){
+            return 3;
+        },
+        canGoNext(){
+             return  this.index <  this.lenght - 1;
+        },
+        canGoPrev(){
+             return  this.index > 0;
+        },
+        leftPosition(){
+            const unit = '%';
+            const left = -100 * this.index;
+            return left + unit;
+        }
+    },
+    methods:{
+        prev () {
+            if(!canGoPrev) return;
+            this.index -=1;
+       },
+        next () {
+            if(!canGoNext) return;
+            this.index +=1;
+
+        }
+    }
+};
+</script>
+ <style src= './image-slider.css'/>
+
+<template>
+ <div class ="component-image-slider">
+         <div class = "slides">
+                <picture v-bind:style="{ left: leftPosition }" class="slide" >
+                 <img src= "http://res.cloudinary.com/constgen/image/upload/w_600,h_470,c_fill/Cars/1411800820_nissangt-r_wm_carreveal_week6_forzahorizon21.jpg" alt="">
+                     </picture>
+                 <picture class="slide">
+                     <img src= "http://res.cloudinary.com/constgen/image/upload/w_600,h_470,c_fill/Cars/ChevroletCorvetteZO6_MayDLC_ForzaHorizon2_01_WM.jpg" alt="">
+                </picture>
+                 <picture class="slide">
+                 <img src= "http://res.cloudinary.com/constgen/image/upload/w_600,h_470,c_fill/Cars/14442c5e-1063-48d9-9850-537615e0333a.jpg.jpg" alt="">
+                    </picture>
+                
+         </div>
+         <button @click="prev" class="prev">&#100094;</button>
+         <button @click="next" class="next">&#100095; </button>
+ </div>
+
+</template>
+
